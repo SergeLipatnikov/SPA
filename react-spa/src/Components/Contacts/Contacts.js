ddad9003f00e582cf824fffb8115ec95123e4c4e
SergeLipatnikov <qiu005b@gmail.com>
2022-04-04 23:47:26 +0300

    1.02

--- react-spa/src/Components/Contacts/Contacts.js
+++ react-spa/src/Components/Contacts/Contacts.js
@@ -1,24 +1,16 @@
-import tellogo from '../Image/telegram-svgrepo-com.svg';
-import whatlogo from '../Image/whatsapp-svgrepo-com.svg';
+import Connect from '../Contacts/Connect/Connect';
 import '../Contacts/contacts.sass';
 
 function Contacts() {
     return (
         <div className='contacts'>
-        <div className='tel'>
-            <img className='tellogo' src={tellogo}/>
-            <p>@lips</p>
+            <div>
+                <Connect/>
+            </div>
+            <div>
+
+            </div>
         </div>
-        <div className='tel'>
-            <img className='tellogo' src={whatlogo}/>
-            <p>@lips</p>
-        </div>
-        <div className='tel'>
-            <img className='tellogo' src={tellogo}/>
-            <p>@lips</p>
-        </div>
-        
-    </div>
     )
   }
   
